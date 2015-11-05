bdf7b8110754f41f4ea5b9d7dc010d843b1b3648
Paul Thompson <nossidge@gmail.com>
2015-11-05 17:33:53 +0000

    can now seed with a string to generate a consistent face

--- face_quote.rb
+++ face_quote.rb
@@ -28,6 +28,7 @@ lines        = ARGV[2] ? ARGV[2].to_i : 2
 zoom         = ARGV[3] ? ARGV[3].to_i : 1
 zoom_face    = ARGV[4] ? ARGV[4].to_i : 1
 zoom_speech  = ARGV[5] ? ARGV[5].to_i : 1
+rand_seed    = ARGV[6] ? ARGV[6]      : nil
 
 ################################################################################
 
@@ -36,7 +37,7 @@ filename_face   = 'new_face.png'
 filename_speech = 'new_speech.png'
 
 # Make a new face.
-Faces::new(filename_face,zoom_face)
+Faces::new(filename_face,zoom_face,rand_seed)
 
 # Make a new speech balloon.
 font = PixelFont.new
--- faces.rb
+++ faces.rb
@@ -13,19 +13,20 @@ module Faces
 
 	PATH = "#{File.dirname(__FILE__)}/face_imgs/"
 
-	def self.random_file(part_name)
+	def self.random_file(part_name, rand_seed=nil)
 		files = Dir["#{PATH}#{part_name}_*.png"]
+		srand(rand_seed.to_s.sum) if rand_seed
 		files.sample
 	end
 
-	def self.new(filename, zoom=1)
+	def self.new(filename, zoom=1, rand_seed=nil)
 		base = ChunkyPNG::Image.from_file PATH + 'base.png'
-		body = ChunkyPNG::Image.from_file random_file('body')
-		face = ChunkyPNG::Image.from_file random_file('face')
-		eyes = ChunkyPNG::Image.from_file random_file('eyes')
-		mout = ChunkyPNG::Image.from_file random_file('mouth')
-		nose = ChunkyPNG::Image.from_file random_file('nose')
-		hair = ChunkyPNG::Image.from_file random_file('hair')
+		body = ChunkyPNG::Image.from_file random_file('body',rand_seed)
+		face = ChunkyPNG::Image.from_file random_file('face',rand_seed)
+		eyes = ChunkyPNG::Image.from_file random_file('eyes',rand_seed)
+		mout = ChunkyPNG::Image.from_file random_file('mouth',rand_seed)
+		nose = ChunkyPNG::Image.from_file random_file('nose',rand_seed)
+		hair = ChunkyPNG::Image.from_file random_file('hair',rand_seed)
 
 		base.compose!(body, 0, 0)
 		base.compose!(face, 0, 0)
